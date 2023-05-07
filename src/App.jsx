beea0282c0d1818daa1315fae41df7f23100a588
hadri12velay <hadri1.2velay@gmail.com>
2023-05-07 15:49:05 +0100

    update to style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Board from "./components/Board";
 import Player from "./components/Player";
 import Popup from "./components/Popup";
@@ -15,9 +15,24 @@ function App() {
     [-2, -4, -3, -5, -6, -3, -4, -2],
   ]);
   const [showPopup, setShowPopup] = useState(false);
+  const [isLandscape, setIsLandscape] = useState(false);
+
+  useEffect(() => {
+    const handleOrientationChange = (e) => {
+      setIsLandscape(e.matches);
+    };
+
+    const mediaQuery = window.matchMedia("(orientation: landscape)");
+    setIsLandscape(mediaQuery.matches);
+    mediaQuery.addEventListener("change", handleOrientationChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleOrientationChange);
+    };
+  }, []);
 
   return (
-    <div id="main">
+    <div id="main" className={isLandscape ? "landscape" : "portrait"}>
       <Player player={"player1"} setShowPopup={setShowPopup} />
       <Board board={board} setBoard={setBoard} />
       <Player player={"player2"} setShowPopup={setShowPopup} />
